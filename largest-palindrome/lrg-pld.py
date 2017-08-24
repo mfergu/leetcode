509ac277211441718c863647629cefa0bbb74bff
mfergu <mfergu3@g.clemson.edu>
2017-08-24 18:59:55 -0400

    initial commit

---
+++ largest-palindrome/lrg-pld.py
@@ -0,0 +1,25 @@
+#find the largest palindrome made from the product of two n-digit numbers
+#author suggests result could be too large so mod the value with 1337
+#uses python2
+
+def largestPalindrome(n):
+
+    def test_palindrome(string):
+        rev = string[::-1]
+        i = 0
+        while i < (len(string) / 2):
+            if(string[i] != rev[i]):
+                return False
+            i += 1
+        return True
+
+    buff = []
+    for i in range( ( n*10)-1, 1, -1):
+        for j in range( ( n*10)-1, 1, -1):
+            temp = i*j
+            if( test_palindrome(str(temp))):
+                buff.append(temp)
+
+    large_pal = max(buff)
+    return large_pal % 1337 
+
